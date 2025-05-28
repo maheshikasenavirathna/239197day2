b9389c615513de89444de942bb27985764d9d3f4
Geshan Anupama <129172291+Geshananupama@users.noreply.github.com>
2025-05-28 19:05:40 +0530

    function

---
+++ ex2.py
@@ -0,0 +1,3 @@
+def sum(a,b):
+    print(" the sum of numbers is ",a+b)
+sum(8,9)
---
+++ q2.py
@@ -0,0 +1,9 @@
+def addition(a,b):
+    if (a>b):
+     return a
+    else:
+     return b
+    print(f"larger number is {number}",a>b)
+addition(5,9)
+
+    
