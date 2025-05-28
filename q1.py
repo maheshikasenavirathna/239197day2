ab71ad86c8a8631addedf6158923d62bd851c052
Geshan Anupama <129172291+Geshananupama@users.noreply.github.com>
2025-05-28 18:12:23 +0530

    fdf

--- q1.py
+++ q1.py
@@ -3,4 +3,4 @@ for i in range(1,11,1):
     sqr=i*i
     if ((sqr%4==0)):
         total+=sqr    
-print("sum of squre numbers of numbers 1 to 10 is:",total)
+print("sum of squre numbers of numbers 1 to 1100 is:",total)
