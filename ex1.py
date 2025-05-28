e95f098ab42d551c9e96dcbffbf0c6c15ca46b00
Geshan Anupama <129172291+Geshananupama@users.noreply.github.com>
2025-05-28 17:51:35 +0530

    first commit

---
+++ ex1.py
@@ -0,0 +1,6 @@
+
+for x in range(1,11,1):
+    print(x)
+p=[1,3,7,9,"maheshika"]
+for t in p:
+    print (t)
---
+++ q1.py
@@ -0,0 +1,6 @@
+total=0
+for i in range(1,11,1):
+    sqr=i*i
+    if ((sqr%4==0)):
+        total+=sqr    
+print("sum of squre numbers of numbers 1 to 10 is:",total)
