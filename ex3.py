c8e549a87ce47d5dce8fa9c39d0d8f9e33e7b007
Geshan Anupama <129172291+Geshananupama@users.noreply.github.com>
2025-05-28 20:13:50 +0530

    long

--- ex3.py
+++ ex3.py
@@ -13,26 +13,26 @@ class LinkedList:
         while printaval is not None:
             print(printaval.data)
             printaval = printaval.next
-    def _inset_at_Begining_(self,newdata):
+    def _inset_at_Begining(self,newdata):
         newNode= Node(newdata)
         newNode.next=self.head
         self.head=newNode
     
 
 
-l1=LinkedList()
-l1.head=Node("Toyota")
+ll=LinkedList()
+ll.head=Node("Toyota")
 l2=Node("BMW")
 l3=Node("Audi")
 l4=Node("Lambogini")
-l1.head.next=l2
+ll.head.next=l2
 l2.next=l3
 l3.next=l4
 
-l1.listprint()
+ll.listprint()
 print("")
-l1._inset_at_Begining("benz")
-l1.listprint()
-l1._inset_at_Begining("chr")
+ll._inset_at_Begining("benz")
+ll.listprint()
+ll._inset_at_Begining("chr")
 print("")
-l1.listprint()
+ll.listprint()
